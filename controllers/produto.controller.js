d06d24266f16758f80eb177d2764d510877051cc
thiagoeu <araujo.thiago1051@gmail.com>
2025-04-06 21:18:37 -0300

    feat: delete

--- controllers/produto.controller.js
+++ controllers/produto.controller.js
@@ -25,14 +25,17 @@ export const createProdutoController = async (req, res) => {
         .json({ error: "Todos os campos são obrigatórios" });
     }
 
+    // Verificar se o produto já existe
     const existingProduct = await Produto.findOne({
       where: { product_code },
     });
 
     if (existingProduct) {
-      return res
-        .status(400)
-        .json({ error: "Já existe um produto com o mesmo código" });
+      return res.status(400).json({
+        message: "O produto já possui um cadastro com esse codigo",
+        error: true,
+        success: false,
+      });
     }
 
     const produto = await Produto.create({
@@ -48,14 +51,40 @@ export const createProdutoController = async (req, res) => {
     res.json({
       message: "Produto cadastrado com sucesso!",
       error: false,
-      sucecss: true,
+      success: true,
     });
   } catch (error) {
     console.error(error);
     res.status(500).json({
       message: error.message || error,
       error: true,
-      sucecss: false,
+      success: false,
     });
+  }
+};
+
+export const deleteProdutoController = async (req, res) => {
+  try {
+    const { product_code } = req.params;
+    if (!product_code) {
+      return res.status(400).json({
+        message: "codigo do produto obrigatório",
+        error: true,
+        success: false,
+      });
+    }
+
+    const produto = await Produto.findOne({ where: { product_code } });
+
+    if (!produto) {
+      return res.status(404).json({ message: "Produto não encontrado" });
+    }
+
+    await produto.destroy();
+
+    res.json({ message: "Produto deletado com sucesso!" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: error.message || error });
   }
 };
--- routes/produto.route.js
+++ routes/produto.route.js
@@ -1,7 +1,12 @@
 import { Router } from "express";
-import { createProdutoController } from "../controllers/produto.controller.js";
+import {
+  createProdutoController,
+  deleteProdutoController,
+} from "../controllers/produto.controller.js";
+
 const produtoRouter = Router();
 
 produtoRouter.post("/cadastro", createProdutoController);
+produtoRouter.delete("/delete/:product_code", deleteProdutoController);
 
 export default produtoRouter;
